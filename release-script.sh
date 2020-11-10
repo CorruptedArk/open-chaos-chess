2fb4b25634ecdc3c4f8ca8130cbc67caaac29b29
CorruptedArk <noahstandingford@gmail.com>
2020-11-09 23:31:36 -0500

    Add files to make releases easier in the future

---
+++ release-script.sh
@@ -0,0 +1,6 @@
+version_name=`grep 'versionName' app/build.gradle | awk '{for(i=1;i<=NF;i++){ if($i ~ /[0-9].*[0-9]/){print $i} } }' | tr -d \"`
+cp release-template.md v$version_name.md
+vim v$version_name.md
+read -p "Enter the release title: " release_title
+#gh release create v$version_name -F v$version_name.md -t "$release_title" "$PWD/app/release/dev.corruptedark.openchaoschess-$version_name-release.apk"
+rm v$version_name.md
