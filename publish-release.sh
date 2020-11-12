ab7f91055ca6e258ad931b439174b8c07a5214ca
CorruptedArk <noahstandingford@gmail.com>
2020-11-11 21:57:04 -0500

    Add change to MainActivity knight to changelog
    Uncomment gh command in publish-release.sh

--- publish-release.sh
+++ publish-release.sh
@@ -2,5 +2,5 @@ version_name=`grep 'versionName' app/build.gradle | awk '{for(i=1;i<=NF;i++){ if
 cp release-template.md v$version_name.md
 vim v$version_name.md
 read -p "Enter the release title: " release_title
-#gh release create v$version_name -F v$version_name.md -t "$release_title" "$PWD/app/release/dev.corruptedark.openchaoschess-$version_name-release.apk"
+gh release create v$version_name -F v$version_name.md -t "$release_title" "$PWD/app/release/dev.corruptedark.openchaoschess-$version_name-release.apk"
 rm v$version_name.md
