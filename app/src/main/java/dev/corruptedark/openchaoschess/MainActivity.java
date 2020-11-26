e8e9b66f0038d3f30dea91f5eecb090181f155ea
CorruptedArk <noahstandingford@gmail.com>
2020-11-26 01:57:50 -0500

    Update to v1.6.5

--- app/src/main/java/dev/corruptedark/openchaoschess/MainActivity.java
+++ app/src/main/java/dev/corruptedark/openchaoschess/MainActivity.java
@@ -100,8 +100,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
