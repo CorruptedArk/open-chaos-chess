06b7049e9095dce207ddb29bd6283d4e70dbe97c
CorruptedArk <noahstandingford@gmail.com>
2020-12-02 03:03:47 -0500

    Make publish-release.sh use mktemp for temp files
    Make publish-release.sh use a temp file for entering the release title

--- publish-release.sh
+++ publish-release.sh
@@ -1,6 +1,11 @@
 version_name=`grep 'versionName' app/build.gradle | awk '{for(i=1;i<=NF;i++){ if($i ~ /[0-9].*[0-9]/){print $i} } }' | tr -d \"`
-cp release-template.md v$version_name.md
-vim v$version_name.md
-read -p "Enter the release title: " release_title
-gh release create v$version_name -F v$version_name.md -t "$release_title" "$PWD/app/release/dev.corruptedark.openchaoschess-$version_name-release.apk"
-rm v$version_name.md
+notes_file=`mktemp XXXXXXXXXX.v$version_name.md`
+cp release-template.md $notes_file
+vim $notes_file
+title_file=`mktemp XXXXXXXXXX.release_title.txt`
+vim $title_file
+release_title=`cat $title_file`
+echo "$release_title"
+gh release create v$version_name -F $notes_file -t "$release_title" "$PWD/app/release/dev.corruptedark.openchaoschess-$version_name-release.apk"
+rm $notes_file
+rm $title_file
