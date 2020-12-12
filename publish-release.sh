a94957b300e8c038acbd0054694b7180cfd8af42
CorruptedArk <noahstandingford@gmail.com>
2020-12-12 03:13:47 -0500

    Create gradle build type gitRelease
    Remove signing config from release to fix compatibility with F-Droid
    Update publish-release.sh to use new build type

--- publish-release.sh
+++ publish-release.sh
@@ -1,4 +1,4 @@
-./gradlew :app:assembleRelease
+./gradlew :app:assembleGitRelease
 version_name=`grep 'versionName' app/build.gradle | awk '{for(i=1;i<=NF;i++){ if($i ~ /[0-9].*[0-9]/){print $i} } }' | tr -d \"`
 notes_file=`mktemp XXXXXXXXXX.v$version_name.md`
 cp release-template.md $notes_file
@@ -7,6 +7,6 @@ title_file=`mktemp XXXXXXXXXX.release_title.txt`
 vim $title_file
 release_title=`cat $title_file`
 echo "$release_title"
-gh release create v$version_name -F $notes_file -t "$release_title" "$PWD/app/build/outputs/apk/release/dev.corruptedark.openchaoschess-$version_name-release.apk"
+gh release create v$version_name -F $notes_file -t "$release_title" "$PWD/app/build/outputs/apk/gitRelease/dev.corruptedark.openchaoschess-$version_name-gitRelease.apk"
 rm $notes_file
 rm $title_file
